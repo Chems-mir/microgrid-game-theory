d5bac0c9b93991f07d046a40cfa1aa7b36f38eb4
Chemsedine Bchir <chemsedine.bchir@gmail.com>
2024-02-29 14:31:13 +0100

    modif micrgrid.py correction methode tournoi pour ne pas inclure des players lorsqu'il ne devrait pas l'être et ajoutdes attribut microgrid_statu et microgrid_energy

--- microgrid.py
+++ microgrid.py
@@ -1,4 +1,14 @@
-class microgrid():
+import Joueur as J
+import Trx as T
+import random
+from ray.rllib.env.multi_agent_env import MultiAgentEnv
+from gym.spaces import Discrete, Box
+import numpy as np
+import copy
+import random
+
+
+class Microgrid():
     def __init__(self, config):
         self.nb_agents = config['n']
         self.L_conso = config['L_conso']
@@ -7,10 +17,10 @@ class microgrid():
         
         self.eval = False
         self.agents = {} #contient les joueurs en tant qu'objet
-        Joueur.cpt =0
+        J.cpt =0
         #J(demand, produced, localisation, action):
         for i in range(self.nb_agents):
-            h = Joueur(self.L_conso[i][0], self.L_prod[i][0], [0,1])
+            h = J.Joueur(self.L_conso[i][0], self.L_prod[i][0], [0,1])
             self.agents[i] = h
 
         self._agents_ids = set(self.agents.keys())
@@ -36,10 +46,13 @@ class microgrid():
         self.Demand_total = sum(buyer.demand for buyer in self.liste_buyers)
         self.Supply_total = sum(seller.supply for seller in self.liste_sellers)
         self.avg_price = 0
+        self.microgrid_statu = 'ok' #can be 'ok', 'shortage', 'surplus'
+        self.microgrid_energy = 0 #contains the need or surplus of energy for the microgrid after the trx within its members.
 
         self.action_space = Discrete(len(self.liste_prix))
         self.observation_space = Box(low=np.array([-1, 0 ,0, 0, 0, 0, 0], dtype=np.float32), high=np.array([1, 10, 100, 100, 100, 100, 1], dtype=np.float32), shape=(7,), dtype=np.float32)
         self.info = {i: {} for i in self._agents_ids}
+
 
 
     def get_observation(self, agent_id):
@@ -75,10 +88,10 @@ class microgrid():
 
         self.agents = {} #contient les joueurs en tant qu'objet
 
-        Joueur.cpt =0
+        J.Joueur.cpt =0
         #J(demand, produced, localisation, action):
         for i in range(self.nb_agents):
-            h = Joueur(self.L_conso[i][0], self.L_prod[i][0], [0,1])
+            h = J.Joueur(self.L_conso[i][0], self.L_prod[i][0], [0,1])
             self.agents[i] = h
 
         self.nb_agents = len(self.agents)
@@ -167,42 +180,33 @@ class microgrid():
         else:
             return 0
 
-    def tournoi_selection(self, liste, target):
-        lst = copy.copy(liste)
-        if len(lst) < 1:
-            print("pas de players")
+
+    def find_closest_to_target(liste, target):
+        if not liste:
             return None
+        else:
+            return min(liste, key=lambda obj: abs(obj.price - target))
 
-        while len(lst) != 1:
-            qualified = []
-            while(lst):
-                if len(lst) >= 2:
-                    fight1 = random.sample(lst, 2)
-                    ## print("fight :", fight1)
-                    if abs(fight1[0].price - target) < abs(fight1[1].price - target):
-                        qualified.append(fight1[0])
-                    else:
-                        qualified.append(fight1[1])
-
-                    lst = list(set(lst) - set(fight1))
-
-                else:
-                    qualified.append(lst[0])
-                    lst = []
-
-            lst = qualified
-
-        return lst[0]
 
     def find_closest_pair(self, team1, team2):
-        closest_pair = []
+        team1.sort(key=lambda obj: obj.price) 
+        team2.sort(key=lambda obj: obj.price) 
+
+        i, j = 0, 0  # Pointers for team1 and team2
         min_distance = float('inf')
-        for a in team1:
-            for b in team2:
-                distance = abs(a.price - b.price)  # Compute the absolute difference
-                if distance < min_distance:
-                    min_distance = distance
-                    closest_pair = [a, b]
+        closest_pair = None
+
+        while i < len(team1) and j < len(team2):
+            distance = abs(team1[i].price - team2[j].price)
+            if distance < min_distance:
+                min_distance = distance
+                closest_pair = [team1[i], team2[j]]
+
+            # Adjust pointers strategically. If team1[i]'s price is smaller, increment i. Otherwise, increment j 
+            if team1[i].price < team2[j].price:
+                i += 1
+            else:
+                j += 1
 
         return closest_pair
 
@@ -215,20 +219,49 @@ class microgrid():
         D = self.Demand_total
         nb_buyers = len(self.liste_buyers)
         nb_sellers = len(self.liste_sellers)
-        #m_global = self.get_weighted_moy()
 
         if choix == 'var1':
+            team_b1 = copy.copy(self.liste_buyers)
+            team_s1 = copy.copy(self.liste_sellers)
+
             while(len(self.liste_buyers) > 0 and len(self.liste_sellers)>0):
-                team_b1 = copy.copy(self.liste_buyers)
-                team_s1 = copy.copy(self.liste_sellers)
+                if len(team_b1) == 0:
+                    team_b1 = copy.copy(self.liste_buyers)
+                if len(team_s1) == 0:
+                    team_s1 = copy.copy(self.liste_sellers)
 
                 while(len(team_b1) > 0 and len(team_s1)>0):
+                    
+                    #print team b1 et s1 et liste sellers buyers pour les comparer 
+                    #et s'assurer que la mise à jour de chaque joueur est correcte
+                    print("-----")
+                    print("-----")
+                    print("team b1:")
+                    for i in team_b1:
+                        print(i)
+                    print("-----")
+
+                    print("liste_buyers:")
+                    for i in self.liste_buyers:
+                        print(i)
+                    print("-----")
+
+                    print('team s1')
+                    for j in team_s1:
+                        print(j)
+                    print("-----")
+                    print("liste_sellers")
+                    for j in self.liste_sellers:
+                        print(j)
+                    print("-----")
+
+
 
                     player_s = random.choice(team_s1)
                     player_b = random.choice(team_b1)
 
                     if abs(player_s.price - m_global) < abs(player_b.price - m_global):
-                        trx = Trx(player_b, player_s, self.max_qtity_trx(player_b, player_s, self.Supply_total, self.Demand_total, nb_buyers, nb_sellers), player_s.price)
+                        trx = T.Trx(player_b, player_s, self.max_qtity_trx(player_b, player_s, self.Supply_total, self.Demand_total, nb_buyers, nb_sellers), player_s.price)
                         liste_trx.append(trx)
                         #print(trx)
                         player_b.demand -= trx.quantity
@@ -246,7 +279,7 @@ class microgrid():
 
                     elif abs(player_s.price - m_global) > abs(player_b.price - m_global):
 
-                        trx = Trx(player_b, player_s, self.max_qtity_trx(player_b, player_s, self.Supply_total,  self.Demand_total, nb_buyers, nb_sellers), player_b.price)
+                        trx = T.Trx(player_b, player_s, self.max_qtity_trx(player_b, player_s, self.Supply_total,  self.Demand_total, nb_buyers, nb_sellers), player_b.price)
                         liste_trx.append(trx)
                         #print(trx)
                         player_b.demand -= trx.quantity
@@ -261,7 +294,7 @@ class microgrid():
                             self.liste_sellers.remove(player_s)
                     else :
 
-                        trx = Trx(player_b, player_s, self.max_qtity_trx(player_b, player_s, self.Supply_total,  self.Demand_total, nb_buyers, nb_sellers), m_global)
+                        trx = T.Trx(player_b, player_s, self.max_qtity_trx(player_b, player_s, self.Supply_total,  self.Demand_total, nb_buyers, nb_sellers), m_global)
                         liste_trx.append(trx)
                         #print(trx)
                         player_b.demand -= trx.quantity
@@ -288,14 +321,14 @@ class microgrid():
             return liste_trx
 
         elif choix == 'var2':
-            while((len(self.liste_buyers) >0) and (len(team_s) > 0)):
+            while((len(self.liste_buyers) >0) and (len(self.liste_sellers) > 0)):
 
-                winner_buyer = self.tournoi_selection(self.liste_buyers, m_global)
-                winner_seller = self.tournoi_selection(team_s, m_global)
+                winner_buyer = self.find_closest_to_target(self.liste_buyers, m_global)
+                winner_seller = self.find_closest_to_target(self.liste_sellers, m_global)
 
                 if abs(winner_seller.price - m_global) < abs(winner_buyer.price - m_global):
                     #winner = seller
-                    trx = Trx(winner_buyer, winner_seller, max(winner_buyer.demand, winner_seller.supply), winner_seller.price)
+                    trx = T.Trx(winner_buyer, winner_seller, max(winner_buyer.demand, winner_seller.supply), winner_seller.price)
                     liste_trx.append(trx)
                     winner_buyer.demand -= trx.quantity
                     winner_seller.supply -= trx.quantity
@@ -304,11 +337,11 @@ class microgrid():
                     if winner_buyer.demand == 0:
                         self.liste_buyers.remove(winner_buyer)
                     if winner_seller.supply == 0:
-                        team_s.remove(winner_seller)
+                        self.liste_sellers.remove(winner_seller)
 
                 elif abs(winner_seller.price - m_global) > abs(winner_buyer.price - m_global):
                     # winner = player_b
-                    trx = Trx(winner_buyer, winner_seller, max(winner_buyer.demand, winner_seller.supply), winner_buyer.price)
+                    trx = T.Trx(winner_buyer, winner_seller, max(winner_buyer.demand, winner_seller.supply), winner_buyer.price)
                     liste_trx.append(trx)
                     winner_buyer.demand -= trx.quantity
                     winner_seller.supply -= trx.quantity
@@ -317,9 +350,9 @@ class microgrid():
                     if winner_buyer.demand == 0:
                         self.liste_buyers.remove(winner_buyer)
                     if winner_seller.supply == 0:
-                        team_s.remove(winner_seller)
+                        self.liste_sellers.remove(winner_seller)
                 else:
-                    trx = Trx(winner_buyer, winner_seller, max(winner_buyer.demand, winner_seller.supply), m_global)
+                    trx = T.Trx(winner_buyer, winner_seller, max(winner_buyer.demand, winner_seller.supply), m_global)
                     liste_trx.append(trx)
                     winner_buyer.demand -= trx.quantity
                     winner_seller.supply -= trx.quantity
@@ -328,7 +361,7 @@ class microgrid():
                     if winner_buyer.demand == 0:
                         self.liste_buyers.remove(winner_buyer)
                     if winner_seller.supply == 0:
-                        team_s.remove(winner_seller)
+                        self.liste_sellers.remove(winner_seller)
 
 
             self.Demand_total_end = D
@@ -348,7 +381,7 @@ class microgrid():
 
                     if abs(results[1].price - m_global) < abs(results[0].price - m_global):
                         #winner = seller
-                        trx = Trx(results[0], results[1], max(results[0].demand, results[1].supply), results[1].price)
+                        trx = T.Trx(results[0], results[1], max(results[0].demand, results[1].supply), results[1].price)
                         liste_trx.append(trx)
                         results[0].demand -= trx.quantity
                         results[1].supply -= trx.quantity
@@ -357,7 +390,7 @@ class microgrid():
 
                     elif abs(results[1].price - m_global) > abs(results[0].price - m_global):
                         # winner = player_b
-                        trx = Trx(results[0], results[1], max(results[0].demand, results[1].supply), results[0].price)
+                        trx = T.Trx(results[0], results[1], max(results[0].demand, results[1].supply), results[0].price)
                         liste_trx.append(trx)
                         results[0].demand -= trx.quantity
                         results[1].supply -= trx.quantity
@@ -365,7 +398,7 @@ class microgrid():
                         S -= trx.quantity
 
                     else:
-                        trx = Trx(results[0], results[1], max(results[0].demand, results[1].supply), m_global)
+                        trx = T.Trx(results[0], results[1], max(results[0].demand, results[1].supply), m_global)
                         liste_trx.append(trx)
                         results[0].demand -= trx.quantity
                         results[1].supply -= trx.quantity
@@ -393,6 +426,16 @@ class microgrid():
             transactions = self.tournoi(moy)
     # [print(t)  for t in transactions]
 
+        #update statu of microgrid
+        self.microgrid_energy = self.Supply_total - self.Demand_total 
+        if self.microgrid_energy > 0:
+            self.microgrid_statu = 'surplus'
+        elif self.microgrid_energy < 0:
+            self.microgrid_statu = 'shortage'
+        else:
+            self.microgrid_statu = 'ok'
+        
+
         # calcul le payoff u_i(x_i, x__i) du joueur i, avec l'action joué x=(x_1, ...., xn)
         for t in transactions:
             pen_seller = self.penalization(self.agents[t.seller.id].price, moy, 0.1)
@@ -402,3 +445,66 @@ class microgrid():
             L_payoffs[t.buyer.id] += -1*(t.price * t.quantity) - pen_buyer  # - cost(t.quantity/2.)
 
         return L_payoffs
+
+
+if __name__ == '__main__':
+
+    # Set up the microgrid instance with test configuration
+    consomations_H1 = np.array([0.31, 0.292, 0.308, 0.286, 0.29,  0.285, 0.84,  1.82,  2.22,  2.18,  1.9, 1.93, 1.647, 0.998, 1.173, 0.982, 1.262, 1.491, 1.396, 3.311, 2.332, 2.664, 2.212, 0.402])
+    consomations_H2 = np.array([0.283, 0.187, 0.328, 0.265, 0.265, 0.339, 0.681, 3.343, 1.656, 2.57 , 1.867, 1.924, 1.386, 0.847, 1.177, 1.032, 1.009, 1.625, 1.393, 2.533, 2.307, 1.932, 2.665, 0.253])
+    consomations_H3 = np.array([0.185, 0.189, 0.363, 0.242, 0.205, 0.275, 0.55 , 2.986, 2.192, 0.647, 1.826, 1.959, 2.783, 0.635, 1.111, 1.317, 1.224, 1.524, 1.502, 3.494, 3.550, 2.773, 3.694, 0.202])
+    consomations_H4 = np.array([0.253, 0.204, 0.384, 0.274, 0.318, 0.339, 0.476, 1.754, 1.466, 3.052, 1.86 , 1.929, 1.423, 0.989, 1.186, 0.649, 1.005, 1.482, 1.483, 4.395, 3.034, 2.67 , 1.23 , 0.798])
+    consomations_H5 = np.array([0.108, 0.246, 0.404, 0.205, 0.235, 0.237, 0.615, 2.971, 2.259, 1.716, 2.464, 1.890, 1.941, 1.530, 1.096, 1.305, 1.266, 1.479, 1.387, 3.556, 3.079, 2.725, 3.652, 0.218])
+    consomations_H6 = np.array([0.236, 0.071, 0.356, 0.297, 0.306, 0.360, 0.710, 3.365, 1.644, 2.587, 1.881, 1.849, 1.283, 0.777, 1.116, 0.978, 0.953, 1.601, 1.386, 2.500, 2.162, 1.908, 2.611, 0.118])
+
+    L_conso = [consomations_H1, consomations_H2, consomations_H3, consomations_H4, consomations_H5, consomations_H6]
+
+    produced_solar_H1 = np.array([0]*24)
+    produced_solar_H2 = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.045, 0.23, 1.037, 1.80, 2.32, 2.56, 2.56, 2.18, 1.9, 1.3, 0.43, 0.01, 0.0, 0.0, 0.0, 0.0, 0.0])
+    produced_solar_H3 = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.024, 0.33, 1.12, 1.92, 2.36, 2.66, 2.7, 2.5, 1.97, 1.5, 0.51, 0.132, 0.00, 0.0, 0.0, 0.0, 0.0 ])
+    produced_solar_H4 = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.001, 0.04, 0.40, 1.054, 1.88, 2.43, 2.67, 2.85, 2.65, 1.97, 1.5, 0.66, 0.067, 0.008, 0.0, 0.0, 0.0, 0.0] )
+    produced_solar_H5 = np.array([0]*24)
+    produced_solar_H6 =  np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.034, 0.38, 2.0, 2.92, 2.56, 2.76, 3.5, 3.2, 2.97, 2.5, 0.91, 0.2, 0.10, 0.0, 0.0, 0.0, 0.0 ])
+
+
+    L_solar = [produced_solar_H1, produced_solar_H2, produced_solar_H3, produced_solar_H4, produced_solar_H5, produced_solar_H6]
+
+    produced_eolien_H1 = np.array([0]*24)
+    produced_eolien_H2 = np.array([0.247, 0.242, 0.217, 0.199, 0.18 , 0.159, 0.156, 0.152, 0.134, 0.095, 0.07 , 0.073, 0.065, 0.058, 0.056, 0.057, 0.056, 0.057, 0.067, 0.077, 0.102, 0.126, 0.135, 0.153])*2
+    produced_eolien_H3 = np.array([0.238, 0.233, 0.23 , 0.198, 0.182, 0.164, 0.152, 0.15 , 0.138, 0.111, 0.088, 0.071, 0.065, 0.056, 0.056, 0.055, 0.054, 0.056, 0.061, 0.077, 0.1  , 0.132, 0.136, 0.149])*2
+    produced_eolien_H4 = np.array([0.259, 0.252, 0.23 , 0.215, 0.19 , 0.17 , 0.164, 0.159, 0.146, 0.119, 0.083, 0.079, 0.069, 0.065, 0.057, 0.059, 0.059, 0.06 , 0.07 , 0.084, 0.098, 0.131, 0.14 , 0.155])*2
+    produced_eolien_H5 = np.array([0]*24)
+    produced_eolien_H6 = np.array([0.28, 0.27, 0.27 , 0.238, 0.22, 0.194, 0.182, 0.17 , 0.17, 0.15, 0.13, 0.10, 0.085, 0.066, 0.06, 0.06, 0.055, 0.0656, 0.0761, 0.087, 0.13  , 0.152, 0.156, 0.16])*2
+
+
+    L_eolien = [produced_eolien_H1, produced_eolien_H2, produced_eolien_H3, produced_eolien_H4, produced_eolien_H5, produced_eolien_H6]
+    price_sp_rachat = 0.06
+    price_sp =  0.28
+    step = 0.01
+
+    liste_prix = np.linspace(price_sp_rachat, price_sp, num = int((price_sp - price_sp_rachat)/step) +1 )
+    demand_total_prec = 3.54
+    supply_total_prec = 4.2
+    avg_price_old = (price_sp_rachat + price_sp)/2.
+
+
+    env_config = {
+            "n": len(L_eolien),
+            "liste_prix" : liste_prix,
+            "demand_total_prec" : demand_total_prec,
+            "supply_total_prec": supply_total_prec,
+            "avg_price_old": avg_price_old,
+            "L_conso" : L_conso,
+            "L_solar": L_solar,
+            "L_eolien": L_eolien
+        }
+
+
+    env = Microgrid(env_config)
+    for identifiant, agent in env.agents.items():
+        agent.price = random.choice(liste_prix)
+
+    moyenne = env.get_weighted_moy()
+    liste_trx = env.tournoi(moyenne)
+    for k in liste_trx:
+        print(k)
